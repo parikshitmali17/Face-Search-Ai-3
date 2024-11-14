b8b4e38401bb3965665eca6e81a66923d6c666a6
Parikshit <Parikshitmali17@gmail.com>
2024-11-14 16:35:14 +0530

    Text Added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -40,7 +40,7 @@ export default function Card(){
         style={{ width: '100%', height: '225px', objectFit: 'cover' }}
       />
             <div class="card-body">
-              <p class="card-text">Get Email and Phone Number of an Individual using Name</p>
+              <p class="card-text">Instantly Find Email Address and Phone Number of any Individual using their Full Name Easily and Quickly Online Searches</p>
               {/* <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
@@ -61,7 +61,7 @@ export default function Card(){
       />
             {/* <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> */}
             <div class="card-body">
-              <p class="card-text">Get to Know about someone their Name, Topics to Discuss with an customized poem</p>
+              <p class="card-text">Discover Someone's Interests and Get Topics to Discuss with Them by Their Name, Plus a Personalized Customized Poem Insight</p>
               {/* <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>

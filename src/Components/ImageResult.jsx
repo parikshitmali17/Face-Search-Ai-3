53177e0101624bb0a414240052cd5eba61f19ab2
Parikshit <Parikshitmali17@gmail.com>
2024-11-17 14:59:38 +0530

    link

--- src/Components/ImageResult.jsx
+++ src/Components/ImageResult.jsx
@@ -20,8 +20,8 @@ function ImageResult() {
     {/* Flex container to align cards in a row */}
     <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
     
+      <SearchCard src="https://www.google.com/imgres?q=joe%20biden&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F68%2FJoe_Biden_presidential_portrait.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJoe_Biden&docid=n3VthzSMI2HkHM&tbnid=KzmnocMkP1Zx6M&vet=12ahUKEwibucXPhuOJAxUdxTgGHXqXBboQM3oECBUQAA..i&w=2400&h=3000&hcb=2&ved=2ahUKEwibucXPhuOJAxUdxTgGHXqXBboQM3oECBUQAA" />
       <SearchCard src="/Images/download.JPEG" />
-      
      
       {/* <SearchCard src="/Images/JB2.JPEG" /> */}
       <SearchCard src="/Images/JB3.JPEG" />

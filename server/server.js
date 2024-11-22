024662f32c07cda04908506673dcf02c88417dc2
Parikshit <Parikshitmali17@gmail.com>
2024-11-22 10:31:17 +0530

    Remove Unnecessary comment

--- server/server.js
+++ server/server.js
@@ -1,90 +1,4 @@
 
-// const express = require('express');
-// const cors = require('cors');
-// const stripe = require('stripe')('sk_test_51QKhjY05k7GGw7uekqfzzhGbTZDPtdoAroVTMxRZ6Skgq4W9bitQt5eMOQDOSCYOO7pVGtocXVDGC20GMSqVgXOP00YjyaJBC1');
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// });
-
-// app.post('/payment', async (req, res) => {
-//     try {
-//         const product = await stripe.products.create({
-//             name: "T-Shirt",
-//         });
-
-//         const price = await stripe.prices.create({
-//             product: product.id,
-//             unit_amount: 100 * 100, // 100 INR
-//             currency: 'inr',
-//         });
-
-//         const session = await stripe.checkout.sessions.create({
-//             line_items: [
-//                 {
-//                     price: price.id,
-//                     quantity: 1,
-//                 }
-//             ],
-//             mode: 'payment',
-//             success_url: 'http://localhost:3000/success',
-//             cancel_url: 'http://localhost:3000/cancel',
-//             customer_email: 'demo@gmail.com',
-//         });
-
-//         res.json({ url: session.url });
-//     } catch (error) {
-//         console.error('Error creating payment session:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
-// app.get('/success',(req,res)=>{
-//   res.send("Payment Succefully done");
-// })
-
-// app.get('/cancel',(req,res)=>{
-//   res.send("Your Payment is Cancled");
-// })
-
-// app.listen(3000, () => {
-//     console.log('Server running on port 3000');
-// });
-
-
-
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'Face_Search_AI',
-//   password: 'IITsonly@17',
-//   port: 5432,
-// });
-
-// module.exports = pool;
-
-
-// app.get('/users', async (req, res) => {
-//   try {
-//     const result = await pool.query('SELECT * FROM users');
-//     res.json(result.rows);
-//     console.log(result.rows);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-// 2nd
-
-
 const express = require('express');
 const cors = require('cors');
 const stripe = require('stripe')('sk_test_51QKhjY05k7GGw7uekqfzzhGbTZDPtdoAroVTMxRZ6Skgq4W9bitQt5eMOQDOSCYOO7pVGtocXVDGC20GMSqVgXOP00YjyaJBC1');
@@ -203,20 +117,6 @@ app.post('/api/deleteUser', async (req, res) => {
   }
 });
 
-// app.post('/api/updateUser', async (req, res) => {
-//   const { updateUser } = req.body;
-//   console.log(req.body);
-//   try {
-//     const result = await pool.query(
-//       'UPDATE users SET name = $1, email = $2, WHERE id=$3; RETURNING *',
-//       [updateUser]
-//     );
-//     res.status(201).json({ message: 'User Deleted Successfully', user: result.rows[0] });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Failed to Delete User' });
-//   }
-// });
 
 app.post('/api/updateUser', async (req, res) => {
   const { id, name, email } = req.body;
